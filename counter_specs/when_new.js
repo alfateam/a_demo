0d465a9fc40c3b23b9a507f991a5e89384b4e23f
Adlan Elm <adlan@timpex.no>
2012-11-02 23:46:58 +0100

    failing test

--- counter_specs/when_new.js
+++ counter_specs/when_new.js
@@ -6,4 +6,6 @@ when(act, c).
 	it('should be an object').
 		assertTrue(typeof c.sut == 'object').
 	it('should have value equal to zero').
-		assertEqual(0, c.sut.value);
+		assertEqual(0, c.sut.value).
+	it('should fail just for fun').
+		assertFail('error');
